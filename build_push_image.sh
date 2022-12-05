67aa9ca3c8128f216e234921e95a082e22656e92
ariwijayaikd <ikadekariwijaya1@gmail.com>
2022-12-05 18:42:35 +0800

    first push Dockerfile, bash, compose

---
+++ build_push_image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Perintah untuk membuat Docker image dari Dockerfile yang tadi dibuat, dengan nama image item-app, dan memiliki tag v1.
+docker build -t item-app:v1 .
+
+# Melihat daftar image di lokal.
+docker images
+
+# Mengubah nama image agar sesuai dengan format Docker Hub (atau GitHub Packages bila menerapkan saran keempat).
+docker image tag item-app:v1 ariwijayaikd/item-app:v1
+
+# Login ke Docker Hub (atau GitHub Packages bila menerapkan saran keempat) via Terminal.
+export PASSWORD_DOCKER_HUB=${{secrets.PASSWORD_DOCKER_HUB}}
+echo $PASSWORD_DOCKER_HUB | docker login -u ariwijayaikd --password-stdin
+# echo $PASSWORD_DOCKER_HUB | docker ghcr.io -u ariwijayaikd --password-stdin
+
+# Mengunggah image ke Docker Hub (atau GitHub Packages bila menerapkan saran keempat).
+docker push ariwijayaikd/item-app:v1
+# docker push ghcr.io/ariwijayaikd/item-app:v1
